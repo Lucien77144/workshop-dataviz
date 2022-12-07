2c12ad57871d66fbe9844c45b4d4b18b1e369449
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2022-12-07 16:17:38 +0100

    fix(): less speed on mousemove

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,15 +15,25 @@ const bloomParams = {
     bloomThreshold: .25,
     bloomRadius: .25
 };
+const speedCamera = {
+    current: 1000,
+    max: 1000,
+    min: 0
+};
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
-const clock = new THREE.Clock();
 
 let unitList = [];
 let lineList = [];
+let selectedUnits = [];
+
+let lastMove = Date.now();
+window.addEventListener( 'mousemove', (e) => {
+    lastMove = Date.now();
+})
 
 window.addEventListener( 'resize', ()=>{
     sizes.width = window.innerWidth;
@@ -39,12 +49,26 @@ document.body.appendChild( renderer.domElement );
 renderer.setPixelRatio( window.devicePixelRatio );
 
 fetch('http://localhost:1234/data/data.json').then(response => {
-    return response.json()
+    return response.json();
 }).then(json => {
     json.users.forEach(data => {
         generateUnit(data);
     });
-    generateLink(unitList[0], unitList[1]);
+});
+
+document.addEventListener('click', (e) => {
+    const mouse = new THREE.Vector2();
+    mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera( mouse, camera );
+
+    const intersects = raycaster.intersectObjects( scene.children );
+    if(intersects.length > 0) {
+        const unit = intersects[0].object;
+        generateLinks(unit);
+    }
 });
 
 camera.position.z = 5;
@@ -68,14 +92,23 @@ composer = new EffectComposer( renderer );
 composer.addPass( renderScene );
 composer.addPass( bloomPass );
 
+let cos = 0;
 const animate = function () {
-    const elapsedTime = clock.getElapsedTime();
+    requestAnimationFrame(animate);
 
-    requestAnimationFrame( animate );
+    if (Date.now() - lastMove > 2000) {
+        if(speedCamera.current < speedCamera.max) speedCamera.current += 10;
+    } else {
+        if(speedCamera.current > speedCamera.min) speedCamera.current -= 20;
+    }
 
-    camera.position.x = Math.cos( Date.now() / 5000 ) * 40;
-    camera.position.z = Math.sin( Date.now() / 5000 ) * 40;
-    camera.position.y = Math.sin( Date.now() / 5000 ) * 15 + 5;
+
+    const speed = .1+Math.abs((speedCamera.current / 1000));
+    cos += speed*.01;
+
+    camera.position.x = Math.cos( cos ) * 40;
+    camera.position.z = Math.sin( cos ) * 40;
+    camera.position.y = Math.cos( cos ) * 15 + 5;
     
     lineList.forEach(e => {
         if(e.actions.scale < 1) {
@@ -89,46 +122,50 @@ const animate = function () {
 }
 animate();
 
+// ------------------------------//
+//           Functions           //
+// ------------------------------//
 
-
-
-// Functions : 
-function checkCollision(unit1, unit2) {
-    const distance = unit1.position.distanceTo(unit2.position);
-    if (distance < 2) {
-        return true;
-    } else {
-        return false;
-    }
-}
-function generateUnit(data) {
+// Generate :
+function generateUnit(data, color="lightblue") {
     const geometry = new THREE.DodecahedronGeometry( 1, 0 );
     const material = new THREE.MeshStandardMaterial({
-        color: 'lightblue', 
+        color: color, 
     });
-    const cube = new THREE.Mesh( geometry, material );
+    const mesh = new THREE.Mesh( geometry, material );
 
     const limitX = (sizes.width - (sizes.width * .05)) / 20;
     const limitY = (sizes.height - (sizes.height * .05)) / 20;
     
-    cube.position.x = (Math.random() * limitX - limitX / 2) * Math.cos(Math.random() * 360);
-    cube.position.y = (Math.random() * limitY - limitY / 2) * Math.sin(Math.random() * 360);
-    cube.position.z = Math.random() * ((limitY + limitY) / 2) - ((limitY + limitY) / 4);
+    mesh.position.x = (Math.random() * limitX - limitX / 2) * Math.cos(Math.random() * 360);
+    mesh.position.y = (Math.random() * limitY - limitY / 2) * Math.sin(Math.random() * 360);
+    mesh.position.z = Math.random() * ((limitY + limitY) / 2) - ((limitY + limitY) / 4);
 
     for (let i = 0; i < unitList.length; i++) {
-        if(checkCollision(cube, unitList[i])) {
-            return generateUnit();
+        if(checkCollision(mesh, unitList[i])) {
+            return generateUnit(data, color);
         }
     }
 
-    scene.add( cube );
-    unitList.push(cube);
+    mesh.userData = data;
+
+    scene.add( mesh );
+    unitList.push(mesh);
 }
-function generateLink(Mesh1, Mesh2, color="red") {
+function generateLinks(mesh) {
+    const userGroups = [mesh.userData.gender, mesh.userData.format, mesh.userData.platform];
+    userGroups.forEach((group) => {
+        const groupList = unitList.filter(e => Object.values(e.userData).includes(group));
+        groupList.forEach(f => {
+            buildLink(mesh, f, '#ffc9c9');
+        })
+    })
+}
+function buildLink(mesh1, mesh2, color="red") {
     const geometry = new THREE.TubeGeometry(
         new THREE.CatmullRomCurve3([
-            Mesh1.position,
-            Mesh2.position
+            mesh1.position,
+            mesh2.position
         ]),
         64,
         0.05,
@@ -145,9 +182,21 @@ function generateLink(Mesh1, Mesh2, color="red") {
     line.actions = {
         scale: 0,
     };
-    line.originPos = Mesh1.position;
+    line.originPos = mesh1.position;
     lineList.push(line);
 }
+
+// Check :
+function checkCollision(unit1, unit2) {
+    const distance = unit1.position.distanceTo(unit2.position);
+    if (distance < 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Actions : 
 function scaleLine(line) {
     line.scale.set(line.actions.scale, line.actions.scale, line.actions.scale);
     line.position.set(
